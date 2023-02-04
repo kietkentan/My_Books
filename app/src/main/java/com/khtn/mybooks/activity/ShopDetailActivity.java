95a179365802ab1093b10de7809f8562fc02f5f4
Le Kiet <kietkentan@gmail.com>
2023-02-04 12:07:15 +0700

    fixed code

--- app/src/main/java/com/khtn/mybooks/activity/ShopDetailActivity.java
+++ app/src/main/java/com/khtn/mybooks/activity/ShopDetailActivity.java
@@ -275,7 +275,7 @@ public class ShopDetailActivity extends AppCompatActivity implements View.OnClic
             reference.child(mode[Common.modeLogin - 1]).child(Common.currentUser.getId()).child("list_shopFollow").child(String.valueOf(Common.currentUser.getList_shopFollow().size() - 1)).setValue(publisher.getId());
             return;
         }
-        reference.child(mode[Common.modeLogin - 1]).child(Common.currentUser.getId()).child("list_shopFollow").addListenerForSingleValueEvent(new ValueEventListener() {
+        database.getReference("user").child(mode[Common.modeLogin - 1]).child(Common.currentUser.getId()).child("list_shopFollow").addListenerForSingleValueEvent(new ValueEventListener() {
             @SuppressLint("DefaultLocale")
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
